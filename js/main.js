7f52a8910b8b889d867601fd7b4373835b780dd3
joanna-wieczorek <korzeniewskawieczorek@wp.pl>
2025-05-03 14:09:33 +0200

    naniosłam poprawki w kodzie

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
-let firstNumber = 40;
-console.log(firstNumber);
-let secondNumber = 50;
-console.log(secondNumber);
-let suma = firstNumber + secondNumber;
-console.log(suma)
+//let firstNumber = 40;
+//console.log(firstNumber);
+//let secondNumber = 50;
+//console.log(secondNumber);
+//let suma = firstNumber + secondNumber;
+//console.log(suma)
+
+//let firstNumber = 40;
+//let secondNumber = 50;
+//function multiplay(firstNumber, secondNumber) {
+//   let result = firstNumber + secondNumber;
+//  return result;
+  //}
+//console.log(multiplay)
+//let wynik_mnozenia = multiplay
+//console.log(wynik_mnozenia)
+
+let firstNumber = 40
+let secondNumber = 50
+function wynik_dodawania(firstNumber, secondNumber) {
+    let wynik = firstNumber + secondNumber;
+    return wynik;
+}
+
+console.log(wynik_dodawania(40, 50));
+
+//let kolejna_proba = wynik_dodawania(100, 20);
+
+//console.log(kolejna_proba);
